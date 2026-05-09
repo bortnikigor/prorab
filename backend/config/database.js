630faa1ed8713a5020547f85ed6892582d2eb35e
Igor Bortnik <bortnik.igor@gmail.com>
2026-05-09 08:38:04 +0300

    Fix database config for local development

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,8 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      connectionString: env('DATABASE_URL'),
-      ssl: { rejectUnauthorized: false },
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'prorab_db'),
+      user: env('DATABASE_USERNAME', 'elly'),
+      password: env('DATABASE_PASSWORD', ''),
+      ssl: env.bool('DATABASE_SSL', false),
     },
   },
 });
