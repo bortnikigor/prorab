ea791355cf718a8790c1d492722286940a83bb8b
Igor Bortnik <bortnik.igor@gmail.com>
2026-05-09 08:43:39 +0300

    Fix: use DATABASE_URL on Railway, local config locally

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,13 +1,29 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
+module.exports = ({ env }) => {
+  const client = env('DATABASE_CLIENT', 'postgres');
+  
+  if (env('DATABASE_URL')) {
+    return {
+      connection: {
+        client: 'postgres',
+        connection: {
+          connectionString: env('DATABASE_URL'),
+          ssl: { rejectUnauthorized: false },
+        },
+      },
+    };
+  }
+
+  return {
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'prorab_db'),
-      user: env('DATABASE_USERNAME', 'elly'),
-      password: env('DATABASE_PASSWORD', ''),
-      ssl: env.bool('DATABASE_SSL', false),
+      client,
+      connection: {
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'prorab_db'),
+        user: env('DATABASE_USERNAME', 'elly'),
+        password: env('DATABASE_PASSWORD', ''),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
     },
-  },
-});
+  };
+};
